af1f45be750f1799f5a71da8a1e2521ac8527d7c
VinayakTekade <39454332+VinayakTekade@users.noreply.github.com>
2021-03-19 15:35:12 +0530

    Added cors
    Signed-off-by: VinayakTekade <39454332+VinayakTekade@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const cors = require("cors");
 const admin = require("firebase-admin");
 const db = require("./db");
 
 const app = express();
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // app.use(require("./routes/index"));
 
